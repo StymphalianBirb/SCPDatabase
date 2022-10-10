c61c1e16acbe203373ba5f91990c54c7ad8ef6ba
KobeMallonMarsh <kbmmarsh@gmail.com>
2022-10-11 12:28:50 +1300

    FirstCommit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,113 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown"
+import Nav from "react-bootstrap/Nav";
+import Container from "react-bootstrap/Container";
+import { LinkContainer } from "react-router-bootstrap";
+import { Route, Routes } from "react-router-dom";
+import Item from "./Items";
+import getItems from "./getItems";
+import { useEffect, useState } from "react";
+import Home from "./Home";
 
 function App() {
+  const [itemsState, setState] = useState([]);
+  useEffect(() => getItems(setState), []);
+
+  var items = [];
+  if (itemsState) {
+    items = setItems(itemsState.map(item => addZeroes(item.item)), 0);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Container className="App">
+      <Navbar expand="lg" bg="light">
+        <Container fluid>
+          <LinkContainer to="/home">
+            <Navbar.Brand>
+              SCP Foundation
+            </Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav>
+              <Nav.Item>
+                <LinkContainer to="/home" >
+                  <Nav.Link>Home Page</Nav.Link>
+                </LinkContainer>
+              </Nav.Item>
+              <NavDropdown title="SCP Items" id="scp-items-root">
+                <MapItems items={items} name=""/>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Routes>
+        <Route path="/items/:id" element={<Item />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Container>
+  );
+}
+
+function setItems(items, position) {
+  if (items.length === 1) {
+    return items[0];
+  }    
+  const array = [];
+  const indexes = new Set(items.map(item => parseInt(item.charAt(position))));
+  indexes.forEach(index => {
+    var filtered = items.filter(item => parseInt(item.charAt(position)) === index);
+    array[index] = setItems(filtered, position + 1);
+  });
+  return array;
+}
+
+function MapItems(parameters) {
+  const items = parameters.items;
+  if (items.length === 0) {
+    return (
+      <NavDropdown.Item disabled>
+        Loading...
+      </NavDropdown.Item>
+    )
+  }
+  const outerName = parameters.name;
+  if (typeof items == "string") {
+    return(
+      <LinkContainer to={"items/" + items}>
+        <NavDropdown.Item>SCP-{items}</NavDropdown.Item>
+      </LinkContainer>
+    );
+  }
+  return(
+    <div>
+      {
+        items && items.map((item) => {
+          const name = outerName + items.indexOf(item);
+          return(
+            <NavDropdown title={"SCP-" + addXs(name)} key={"scp-items-" + name} id={"scp-items-" + name} drop="end">
+              <MapItems items={item} name={name} />
+            </NavDropdown>
+          )
+        })
+      }
     </div>
   );
+}
+
+function addZeroes(item) {
+  while (item.length < 4) {
+    item = "0" + item;
+  }
+  return item;
+}
+
+function addXs(name) {
+  while (name.length < 4) {
+    name = name + "x";
+  }
+  return name;
 }
 
 export default App;
---
+++ src/Items.js
@@ -0,0 +1,76 @@
+import { useEffect, useState } from "react";
+import { Accordion } from "react-bootstrap";
+import { useParams } from "react-router-dom";
+import getItems from "./getItems";
+
+
+function Item() {
+    var { id } = useParams();
+    var short = id;
+    while (short.startsWith("0")) {
+        short = short.replace("0", "");
+    }
+
+    const [itemsState, setState] = useState([]);
+    useEffect(() => getItems(setState), []);
+    var found = null;
+    if (typeof itemsState != "undefined") {
+        itemsState.forEach((item) => {
+            if (item.item === short) {
+                found = item;
+            }
+        })
+    }
+    if (found == null) {
+        return(
+            <div>
+                Loading...
+            </div>
+        )
+    }
+    
+    return(
+        <div>
+            <div className="display-3"><b>Item:</b> SCP-{id}</div>
+            <div className="display-4"><b>Item Class:</b> {found.class}</div>
+            <Accordion>
+                <Accordion.Item eventKey="containment">
+                    <Accordion.Header>Special Containment Procedures</Accordion.Header>
+                    <Accordion.Body>
+                        <Format content={found.containment}/>
+                    </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="description">
+                    <Accordion.Header>Description</Accordion.Header>
+                    <Accordion.Body>
+                        <Format content={found.description}/>
+                    </Accordion.Body>
+                </Accordion.Item>
+            </Accordion>
+        </div>
+    )
+}
+
+function Format(params) {
+    console.log(params);
+    var paragraphs = params.content.split("\\n\\n");
+    var parts = [];
+    
+    for (var i = 0; i < paragraphs.length; i++) {
+        parts.push({content: paragraphs[i], key: i})
+    }
+
+    return(
+        <div>
+            {
+                parts.map(value => 
+                    <p key={value.key}>
+                        {value.content}
+                    </p>
+                )
+            }
+        </div>
+    )
+}
+
+export default Item;
